cdce41a62d0402c2c57c7357de034c7d9f3d2410
Julio Francisco Bernardino <juliobernardinoprogramador@gmail.com>
2025-02-03 12:59:16 -0300

    update 2

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ function calcularIdade(dataNascimento) {
     }
     return idade;
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const dataNascimento = '2005-07-31'; // Insira sua data de nascimento aqui (ano-mês-dia)
+    const idade = calcularIdade(dataNascimento);
+    document.getElementById('idade').textContent = idade;
+});
