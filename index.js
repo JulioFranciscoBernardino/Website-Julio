f9ec16a572a83b967d987dbd555f49daf53d6a08
Julio Francisco Bernardino <juliobernardinoprogramador@gmail.com>
2025-02-03 12:41:56 -0300

    Update

---
+++ index.js
@@ -0,0 +1,10 @@
+function calcularIdade(dataNascimento) {
+    const hoje = new Date();
+    const nascimento = new Date(dataNascimento);
+    let idade = hoje.getFullYear() - nascimento.getFullYear();
+    const mes = hoje.getMonth() - nascimento.getMonth();
+    if (mes < 0 || (mes === 0 && hoje.getDate() < nascimento.getDate())) {
+        idade--;
+    }
+    return idade;
+}
